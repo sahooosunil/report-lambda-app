cbf55d772d5405e817ff3ef0bfc5b3d5d868f8f8
“sunil” <sunilsahu0123@gmail.com>
2025-01-31 21:38:31 +0530

    ses

--- reportFn/src/main/java/com/sks/reportFn/config/AwsConfig.java
+++ reportFn/src/main/java/com/sks/reportFn/config/AwsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.ses.SesClient;
 
 @Configuration
 public class AwsConfig {
@@ -12,6 +13,14 @@ public class AwsConfig {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .region(Region.of("ap-south-1"))  // Replace with your region
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public SesClient sesClient() {
+        return SesClient.builder()
                 .region(Region.of("ap-south-1"))  // Replace with your region
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
