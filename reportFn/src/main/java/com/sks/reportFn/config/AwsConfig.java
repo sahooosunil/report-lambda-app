4d3eecc2f7baf77d4e463063f89daef29fd0924c
“sunil” <sunilsahu0123@gmail.com>
2025-01-31 21:26:12 +0530

    kjl

---
+++ reportFn/src/main/java/com/sks/reportFn/config/AwsConfig.java
@@ -0,0 +1,19 @@
+package com.sks.reportFn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class AwsConfig {
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of("ap-south-1"))  // Replace with your region
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
